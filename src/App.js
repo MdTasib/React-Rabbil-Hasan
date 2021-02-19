66e1ceefc710bb528860fdfcd20297c5502677f6
ohidul alam <mdtasib442@gmali.com>
2021-02-19 08:38:33 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// FunctionalComponent import here
+import FunctionalComponent from './components/FunctionalAndClass/FunctionalComponent';
+// ClassComponent import here
+import ClassComponent from './components/FunctionalAndClass/ClassComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunctionalComponent />
+      <ClassComponent />
     </div>
   );
 }
---
+++ src/components/FunctionalAndClass/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FunctionalComponent() {
+    return (
+        <div>
+            <h2>I am from Functional Component</h2>
+        </div>
+    )
+};
+
+export default FunctionalComponent;
